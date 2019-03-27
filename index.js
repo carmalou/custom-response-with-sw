16bfed320d57807fcd587806e89b2699620f351d
cablong1 <carmen.long@hobbylobby.com>
2019-03-27 10:21:30 -0500

    initial calls are there

---
+++ index.js
@@ -0,0 +1,27 @@
+function fetchFayePI() {
+    return window.fetch('https://fayepi.herokuapp.com/romcoms')
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(actualRez) {
+        console.log('actualRez ', actualRez);
+        var ul = document.createElement('ol');
+        for(var i = 0; i < actualRez.length; i++) {
+            var tmp = generateList(actualRez[i]);
+            ul.appendChild(tmp);
+        }
+
+        document.getElementById('contents').appendChild(ul);
+    })
+    .catch(function(err) {
+        console.log('err! ', err);
+    });
+}
+
+function generateList(movie) {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(movie.movie_title));
+    return li;
+}
+
+fetchFayePI();
