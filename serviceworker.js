1806a41d841997ba983ee7992d6ac0bade7e211e
cablong1 <carmen.long@hobbylobby.com>
2019-03-28 16:45:32 -0500

    almost there but something weird is happening

--- serviceworker.js
+++ serviceworker.js
@@ -21,28 +21,44 @@ self.onactivate = function(event) {
 // stack overflow on clone: https://stackoverflow.com/questions/37747332/response-whose-body-is-locked-cannot-be-used-to-respond-to-a-request
 // mdn: https://developer.mozilla.org/en-US/docs/Web/API/Response/clone
 
+// noticing that both the request and response have the originally requested URL. maybe that + a combination of header info is how the request <-> response is matched up??
+
+// response.clone
+
+// caches.put??
+
 self.onfetch = function(event) {
     event.respondWith(
         caches.match(event.request)
-        .then(async function(cachedFiles) {
+        .then(function(cachedFiles) {
             if(cachedFiles) {
-                var rezToReturn =cachedFiles.clone();
-                var tmp = await cachedFiles.text();
-                console.log(cachedFiles);
-                console.log(tmp);
-                console.log(event.request);
-                return rezToReturn;
+                return cachedFiles;
+            } else {
+                fetch(event.request)
+                .then(function(response) {
+                    caches.open('cache-response1')
+                    .then(function(cache) {
+                        var clone = response.clone();
+                        return cache.add(clone);
+                    })
+                    .then(function() {
+                        return response;
+                    })
+                    .catch(function(err) {
+                        console.log('err! ', err);
+                    })
+                })
+
+
+                // var returnRez = newRez.clone();
+
+                // var cache = await caches.open('cache-response1');
+
+                // console.log(cache);
+                // // await cache.add(newRez);
+
+                // return returnRez;
             }
-            // if(cachedFiles) {
-            //     return cachedFiles;
-            // } else {
-            //     // I do a check here to make sure nothing from the app itself is being requested.
-            //     // if something from the app were being requested -- like say the index.html page -- this would be returned instead and we don't want that.
-            //     if(!event.request.url.includes(location.origin)) {
-            //         var init = { "status" : 200 , "statusText" : "I am a custom service worker response!" };
-            //         return new Response(null, init);
-            //     }
-            // }
         })
     )
 }
