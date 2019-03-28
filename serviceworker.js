cc51023229c53875542b8163e93f240bf49631b2
cablong1 <carmen.long@hobbylobby.com>
2019-03-28 15:47:06 -0500

    let me show you how to ruin a sw

--- serviceworker.js
+++ serviceworker.js
@@ -18,20 +18,31 @@ self.onactivate = function(event) {
     console.log('sw is up and running!');
 }
 
+// stack overflow on clone: https://stackoverflow.com/questions/37747332/response-whose-body-is-locked-cannot-be-used-to-respond-to-a-request
+// mdn: https://developer.mozilla.org/en-US/docs/Web/API/Response/clone
+
 self.onfetch = function(event) {
     event.respondWith(
         caches.match(event.request)
-        .then(function(cachedFiles) {
+        .then(async function(cachedFiles) {
             if(cachedFiles) {
-                return cachedFiles;
-            } else {
-                // I do a check here to make sure nothing from the app itself is being requested.
-                // if something from the app were being requested -- like say the index.html page -- this would be returned instead and we don't want that.
-                if(!event.request.url.includes(location.origin)) {
-                    var init = { "status" : 200 , "statusText" : "I am a custom service worker response!" };
-                    return new Response(null, init);
-                }
+                var rezToReturn =cachedFiles.clone();
+                var tmp = await cachedFiles.text();
+                console.log(cachedFiles);
+                console.log(tmp);
+                console.log(event.request);
+                return rezToReturn;
             }
+            // if(cachedFiles) {
+            //     return cachedFiles;
+            // } else {
+            //     // I do a check here to make sure nothing from the app itself is being requested.
+            //     // if something from the app were being requested -- like say the index.html page -- this would be returned instead and we don't want that.
+            //     if(!event.request.url.includes(location.origin)) {
+            //         var init = { "status" : 200 , "statusText" : "I am a custom service worker response!" };
+            //         return new Response(null, init);
+            //     }
+            // }
         })
     )
 }
